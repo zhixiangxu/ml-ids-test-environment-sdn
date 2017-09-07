d6101998d904aaf744ceea2e0d07a229fdf650c6
antonsuba <antonsuba@outlook.com>
2017-09-07 21:00:06 +0800

    Finish initial implementation of adaboost classifier

--- ids-classifier.py
+++ ids-classifier.py
@@ -2,39 +2,61 @@ import os
 import pandas as pd
 from sklearn import ensemble
 
-TRAINING_DATA_PATH = os.path.join('data', 'NSL-KDD')
+DATA_PATH = os.path.join('data', 'NSL-KDD')
+TRAIN_FILE_NAME = 'kdd-train.csv'
+TEST_FILE_NAME = 'kdd-test.csv'
 
-def load_training_data(training_data_path):
-    csv_path = 'data.csv' #os.path.join(training_data_path, 'data.csv')
+def load_data_set(data_path, filename):
+    csv_path = os.path.join(data_path, filename)
     return pd.read_csv(csv_path)
 
-def one_hot_encode_columns(data, column):
-    one_hot_encode = pd.get_dummies(packets[column])
-    data = data.drop(column, axis=1)
-    data = data.join(one_hot_encode)
-    return data
-
-def generate_numpy_arr(dataset, classification):
+def generate_arr(dataset, classification):
     classification_arr = dataset[classification].values
     dataset_arr = dataset.values
 
     return dataset_arr, classification_arr
 
-packets = load_training_data(TRAINING_DATA_PATH)
+def convert_class(x):
+    return int(x != 'normal')
 
+#Load training data
+packets = load_data_set(DATA_PATH, TRAIN_FILE_NAME)
+
+#Clean training data
 last_row = len(packets)
 packets = packets.drop(packets.index[last_row - 1])
 del packets['idk']
 
 packets['duration'] = pd.to_numeric(packets['duration'])
 
-#Convert texts to integer representation
-packets = one_hot_encode_columns(packets, 'protocol_type')
-packets = one_hot_encode_columns(packets, 'service')
-packets = one_hot_encode_columns(packets, 'flag')
+#Load test data
+test_packets = load_data_set(DATA_PATH, TEST_FILE_NAME)
+
+#Clean test data
+last_row = len(test_packets)
+test_packets = test_packets.drop(test_packets.index[last_row - 1])
+del test_packets['idk']
+
+test_packets['class'] = test_packets['class'].apply(convert_class)
+test_packets['duration'] = pd.to_numeric(test_packets['duration'])
+
+#One hot encode features
+train_len = len(packets)
+frames = [packets, test_packets]
+temp = pd.concat(frames, axis=0)
+
+temp_preprocessed = pd.get_dummies(temp)
+
+packets = temp_preprocessed[:train_len]
+test_packets = temp_preprocessed[train_len:]
 
 #Generate arrays
-packets_arr, classification_arr = generate_numpy_arr(packets, 'class')
+packets_arr, classification_arr = generate_arr(packets, 'class')
+test_packets_arr, test_classification_arr = generate_arr(test_packets, 'class')
 
+#Train classifier
 clf = ensemble.AdaBoostClassifier()
 clf.fit(packets_arr, classification_arr)
+
+#Test classifier
+clf.predict([test_packets_arr[29]])
